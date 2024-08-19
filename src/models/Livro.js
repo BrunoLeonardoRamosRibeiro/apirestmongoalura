8803b8a4322de8d9b9f6b03ac62da67b285ca43f
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-08-19 11:29:07 -0300

    Validações do mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,8 +5,20 @@ const livroSchema = new mongoose.Schema(
     id: {type: String},
     titulo: {type: String, required: [true, "O título é obrigatório"]},
     autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O autor é obrigatório"]},
-    editora: {type: String, required: [true, "A editora é obrigatória"]},
-    numeroPaginas: {type: Number}
+    editora: {
+      type: String, 
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      },
+
+    },
+    numeroPaginas: {
+      type: Number,
+      min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}."],
+      max: [5000,"O número de páginas deve estar entre 10 e 5000. fornecido: {VALUE}."],
+    }
   }
 );
 
