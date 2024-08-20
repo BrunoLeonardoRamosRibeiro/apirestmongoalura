cf0d9c2ea1cce4f63c4e65168520ea3dee5a0652
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-08-20 07:35:21 -0300

    Valida Global

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -22,7 +22,7 @@ const livroSchema = new mongoose.Schema(
         validator: (valor) => {
           return valor >= 10 && valor  <= 5000;
         },
-        message: "O número de páginas dve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
       }
     }
   }
